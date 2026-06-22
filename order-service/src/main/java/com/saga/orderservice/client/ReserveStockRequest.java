c61da608c5385a0d4e2e4cf8b5d2d2a31a45d93d
Maheesh09 <pramudithamaheesha@gmail.com>
2026-06-22 16:21:43 +0530

    feat:Add event classes to both order and invntory services

--- order-service/src/main/java/com/saga/orderservice/client/ReserveStockRequest.java
+++
@@ -1,8 +0,0 @@
-package com.saga.orderservice.client;
-
-import java.util.List;
-
-public record ReserveStockRequest(
-        Long orderId,
-        List<StockItemRequest> items
-) {}
--- order-service/src/main/java/com/saga/orderservice/client/StockItemRequest.java
+++
@@ -1,6 +0,0 @@
-package com.saga.orderservice.client;
-
-public record StockItemRequest(
-        Long productId,
-        int quantity
-) {}
